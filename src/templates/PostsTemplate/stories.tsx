c2f8ca3cd5fddd5e5fee4cff7719e5924d9a1ed9
Maurício Sousa <morricedev@gmail.com>
2021-07-30 10:57:33 -0300

    Created PostsTemplate

---
+++ src/templates/PostsTemplate/stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { PostsTemplate, PostsTemplateProps } from '.';
+import mock from './mock';
+
+export default {
+  title: 'PostsTemplate',
+  component: PostsTemplate,
+  args: mock,
+} as Meta<PostsTemplateProps>;
+
+export const Template: Story<PostsTemplateProps> = (args) => {
+  return (
+    <div>
+      <PostsTemplate {...args} />
+    </div>
+  );
+};
