30292d608a4ab5c8ba652c61a697bfe66a6a3a32
Maurício Sousa <morricedev@gmail.com>
2021-07-31 15:43:16 -0300

    Config updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,3 @@
 export default {
-  url: 'https://strapi-landing-pages-project-2.herokuapp.com',
-  siteName: 'Otávio Miranda',
-  defaultSlug: 'landing-page',
   graphqlURL: 'http://localhost:1337/graphql',
 };
