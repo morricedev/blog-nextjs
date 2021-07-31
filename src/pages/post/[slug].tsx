f7117d36c8beb3644df7dd206c920b31b203a3fc
Maurício Sousa <morricedev@gmail.com>
2021-07-31 14:00:17 -0300

    Created post page

---
+++ src/pages/post/[slug].tsx
@@ -0,0 +1,69 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/dist/client/router';
+import Head from 'next/head';
+
+import { loadPosts, StrapiData } from '../../api/loadPosts';
+import { PostTemplate } from '../../templates/PostTemplate';
+
+export default function PostPage({ posts, setting }: StrapiData) {
+  const router = useRouter();
+
+  if (router.isFallback) return <h1>Carregando ...</h1>;
+
+  const post = posts[0];
+
+  return (
+    <>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="description" content={post.excerpt} />
+      </Head>
+      <PostTemplate post={post} settings={setting} />;
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  let data: StrapiData | null = null;
+  let paths = [];
+
+  try {
+    data = await loadPosts();
+    paths = data.posts.map((post) => ({ params: { slug: post.slug } }));
+  } catch (error) {
+    data = null;
+  }
+
+  if (!data || !data.posts || !data.posts.length) {
+    paths = [];
+  }
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<StrapiData> = async (ctx) => {
+  let data = null;
+
+  try {
+    data = await loadPosts({ postSlug: ctx.params.slug as string });
+  } catch (error) {
+    data = null;
+  }
+
+  if (!data || !data.posts || !data.posts.length) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      posts: data.posts,
+      setting: data.setting,
+    },
+    revalidate: 24 * 60 * 60,
+  };
+};
