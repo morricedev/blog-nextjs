44fb89a029d1b7d16d4ffa4a8f50db729aac30a2
Maurício Sousa <morricedev@gmail.com>
2021-07-31 14:00:36 -0300

    Created tag page

---
+++ src/pages/tag/[slug].tsx
@@ -0,0 +1,69 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/dist/client/router';
+import Head from 'next/head';
+
+import {
+  defaultLoadPostsVariables,
+  loadPosts,
+  StrapiData,
+} from '../../api/loadPosts';
+import { PostsTemplate } from '../../templates/PostsTemplate';
+
+export default function TagPage({ posts, setting, variables }: StrapiData) {
+  const router = useRouter();
+
+  if (router.isFallback) return <h1>Carregando ...</h1>;
+
+  const tagName = posts[0].tags.filter(
+    (tag) => tag.slug === router.query.slug,
+  )[0].displayName;
+
+  return (
+    <>
+      <Head>
+        <title>
+          Tag: {tagName} - {setting.blogName}
+        </title>
+        <meta name="description" content={setting.blogDescription} />
+      </Head>
+      <PostsTemplate posts={posts} settings={setting} variables={variables} />
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<StrapiData> = async (ctx) => {
+  let data = null;
+
+  const variables = { tagSlug: ctx.params.slug as string };
+
+  try {
+    data = await loadPosts(variables);
+  } catch (error) {
+    data = null;
+  }
+
+  if (!data || !data.posts || !data.posts.length) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      posts: data.posts,
+      setting: data.setting,
+      variables: {
+        ...defaultLoadPostsVariables,
+        ...variables,
+      },
+    },
+    revalidate: 24 * 60 * 60,
+  };
+};
