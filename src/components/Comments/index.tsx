d04674c99ce53c0864715b711a2ef21dc0e1bb3c
Maurício Sousa <morricedev@gmail.com>
2021-08-01 11:16:46 -0300

    Fixed url in Comments

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -16,7 +16,7 @@ export const Comments = ({ id, slug, title, allowComments }: CommentsProps) => {
       <DiscussionEmbed
         shortname="blog-do-morrice"
         config={{
-          url: `/post/${slug}`,
+          url: `https://blog-nextjs-liart.vercel.app/post/${slug}`,
           identifier: id,
           title,
           language: 'pt_BR',
