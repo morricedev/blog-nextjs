1e712e1f322bc95d58ed204024fc956a8361383f
Maurício Sousa <morricedev@gmail.com>
2021-08-01 13:21:42 -0300

    Tests for ToggleTheme component

---
+++ src/components/ToggleTheme/test.tsx
@@ -0,0 +1,52 @@
+import { fireEvent, screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { ToggleTheme } from '.';
+import { theme } from '../../styles/theme';
+
+describe('<ToggleTheme />', () => {
+  beforeEach(() => {
+    localStorage.setItem('theme', JSON.stringify({ ...theme, name: 'dark' }));
+  });
+
+  afterEach(() => {
+    localStorage.removeItem('theme');
+  });
+
+  it('should render input without theme on localStorage', () => {
+    localStorage.removeItem('theme');
+    renderTheme(<ToggleTheme />);
+
+    const input = screen.getByRole('checkbox');
+    expect(input).not.toBeChecked();
+  });
+
+  it('should render input with default theme', () => {
+    localStorage.setItem('theme', JSON.stringify({ ...theme, name: 'light' }));
+
+    renderTheme(<ToggleTheme />);
+
+    const input = screen.getByRole('checkbox');
+    expect(input).not.toBeChecked();
+  });
+
+  it('should render input checked with dark theme', () => {
+    renderTheme(<ToggleTheme />);
+
+    expect(screen.getByRole('checkbox')).toBeChecked;
+  });
+
+  it('should render input and toggle between theme', () => {
+    const { container } = renderTheme(<ToggleTheme />);
+
+    const label = screen.getByLabelText('Toggle light and dark mode');
+    const input = screen.getByRole('checkbox');
+
+    fireEvent.click(label);
+    expect(input).not.toBeChecked();
+
+    fireEvent.click(label);
+    expect(input).toBeChecked();
+
+    expect(container).toMatchSnapshot();
+  });
+});
