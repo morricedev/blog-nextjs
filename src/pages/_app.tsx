cabcbfeea2c095558701091a683fff6d77b827e4
Maurício Sousa <morricedev@gmail.com>
2021-08-05 18:52:13 -0300

    Add progress bar on navigate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import { AppProps } from 'next/app';
+import NextNprogress from 'nextjs-progressbar';
 
 import { GlobalStyles } from '../styles/global-styles';
 import { BlogThemeProvider } from '../contexts/BlogThemeContext';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <BlogThemeProvider>
       <Component {...pageProps} />
       <GlobalStyles />
+      <NextNprogress
+        color={theme.colors.secondary}
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={5}
+        showOnShallow={true}
+        options={{ showSpinner: false }}
+      />
     </BlogThemeProvider>
   );
 }
